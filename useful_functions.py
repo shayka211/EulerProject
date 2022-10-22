a40b5d2e9b72140e131dbe01a91672f1231ace55
shayka211 <shayka211@gmail.com>
2022-10-22 17:37:57 +0300

    useful_functions.py

--- useful_functions.py
+++ useful_functions.py
@@ -10,3 +10,18 @@ def get_list_of_sets_from_text_separated_by_commas(url):
             float_list = [float(i) for i in my_str]
             all_sets.append(float_list)
     return all_sets
+
+
+
+def all_primes_under_N(max_n):
+    numbers = list(range(3, max_n+1, 2))
+    half = (max_n)//2
+    initial = 4
+
+    for step in range(3, max_n+1, 2):
+        for i in range(initial, half, step):
+            numbers[i-1] = 0
+        initial += 2*(step+1)
+
+        if initial > half:
+            return [2] + list(filter(None, numbers))
